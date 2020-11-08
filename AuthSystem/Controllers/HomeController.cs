94881a276e54349b19a1a3dcbbedb93d35a18352
Shravan Patel <ac94959@avcol.school.nz>
2020-11-09 12:46:05 +1300

    Donation

--- AuthSystem/Controllers/HomeController.cs
+++ AuthSystem/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AuthSystem.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
 
 namespace AuthSystem.Controllers
 {
@@ -37,6 +39,26 @@ namespace AuthSystem.Controllers
         public IActionResult Donation()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Donation(Donation ur)
+        {
+            string connection = "Server=tpisql01.avcol.school.nz ; Database=MVCAuthDB-Shravan;Trusted_Connection=True;MultipleActiveResultSets=true";
+            
+                using(SqlConnection sqlconn=new SqlConnection(connection))
+                {
+                string sqlquery = "insert into Donation(ID,Name,Email,PhoneNumner,Address,Suburb,City,PostCode,DonationAmount,CardNumber,CVV,ExpirationDate) values " +
+                    "('" + ur.ID + "', '" + ur.Name + "','" + ur.Email + "', '" + ur.PhoneNumber + "', '" + ur.Address + "', '" + ur.Suburb + "', '" + ur.City + "', '" + ur.PostCode + "', '" + ur.DonationAmount + "', '" + ur.CardNumber + "', '" + ur.CVV + "', '" + ur.ExpirationDate + "')";
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery,sqlconn))
+                {
+                    sqlconn.Open();
+                    sqlcomm.ExecuteNonQuery();
+                    ViewData["Message"] = "Donation is accepted";
+                    return View(ur);
+                }
+            }
+       
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- AuthSystem/Models/Donation.cs
+++ AuthSystem/Models/Donation.cs
@@ -1,1 +1,69 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AuthSystem.Models
+{
+    public class Donation
+    {
+        //stores data for donation page
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Name")]
+        [Display(Name = "Name: ")] 
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Email")]
+        [Display(Name = "Email:")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Phone Number")]
+        [Display(Name = "PhoneNumber:")]
+        public string PhoneNumber { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Address")]
+        [Display(Name = "Address:")]
+        public string Address { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Suburb")]
+        [Display(Name = "Suburb:")]
+        public string Suburb { get; set; }
+
+
+        [Required(ErrorMessage = "Pleae Enter City")]
+        [Display(Name = "City:")]
+        public string City { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Post Code")]
+        [Display(Name = "PostCode:")]
+        public string PostCode { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Donation Amount")]
+        [Display(Name = "DonationAmount:")]
+        public string DonationAmount { get; set; }
+
+        [Required(ErrorMessage = "Please Enter CardNumber")]
+        [Display(Name = "CardNumber:")]
+        public string CardNumber { get; set; }
+
+        [Required(ErrorMessage = "Please Enter CVV")]
+        [Display(Name = "CVV:")]
+        public string CVV { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter ExpirationDate")]
+        [Display(Name = "ExpirationDate:")]
+        public string ExpirationDate { get; set; }
+        
+    } 
+}
